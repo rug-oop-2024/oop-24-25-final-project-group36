70958c40dc98da1eae92ed296c4b2b2c70ed13be
hihina17 <h.naruse@student.rug.nl>
2024-11-10 00:06:56 +0100

    Fixed style issues on pipeline.py

--- autoop/core/ml/pipeline.py
+++ autoop/core/ml/pipeline.py
@@ -90,28 +90,30 @@ Pipeline(
         )[0]
         self._register_artifact(target_feature_name, artifact)
         input_results = preprocess_features(
-            self._input_features, self._dataset
-            )
+            self._input_features, self._dataset)
         for feature_name, data, artifact in input_results:
             self._register_artifact(feature_name, artifact)
-        # Get the input vectors and output vector, 
+        # Get the input vectors and output vector,
         # sort by feature name for consistency
         self._output_vector = target_data
         self._input_vectors = [
-            data for (feature_name, data, artifact) in input_results
-            ]
+            data for (feature_name, data, artifact) in input_results]
 
     def _split_data(self):
         # Split the data into training and testing sets
         split = self._split
         self._train_X = [
-            vector[: int(split * len(vector))] for vector in self._input_vectors
+            vector[: int(split * len(vector))]
+            for vector in self._input_vectors
         ]
         self._test_X = [
-            vector[int(split * len(vector)) :] for vector in self._input_vectors
+            vector[int(split * len(vector)):]
+            for vector in self._input_vectors
         ]
-        self._train_y = self._output_vector[: int(split * len(self._output_vector))]
-        self._test_y = self._output_vector[int(split * len(self._output_vector)) :]
+        self._train_y = self._output_vector[
+            : int(split * len(self._output_vector))]
+        self._test_y = self._output_vector[
+            int(split * len(self._output_vector)):]
 
     def _compact_vectors(self, vectors: List[np.array]) -> np.array:
         return np.concatenate(vectors, axis=1)
